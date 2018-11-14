23603b84f5b62ac471ea1ae09576ff80f490d9fc
SweepMonk <guangliang1120@gmail.com>
2018-11-14 09:33:46 +0800

    [STAGING][Monk] add githooks

--- src/main/java/com/github/mgljava/staging/service/CalcService.java
+++ src/main/java/com/github/mgljava/staging/service/CalcService.java
@@ -1,5 +1,8 @@
 package com.github.mgljava.staging.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalcService {
 
   public int add(int a, int b) {
--- src/test/java/com/github/mgljava/staging/service/CalcServiceTest.java
+++ src/test/java/com/github/mgljava/staging/service/CalcServiceTest.java
@@ -1,17 +1,27 @@
 package com.github.mgljava.staging.service;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CalcServiceTest {
 
+  @InjectMocks
+  private CalcService calcService;
+
   @Test
-  public void add() {
+  public void should_get_3_when_a_is_1_and_b_is_2() {
     // given
     int a = 1;
     int b = 2;
 
     // when
-
+    final int result = calcService.add(a, b);
     // then
+    assertEquals(3, result);
   }
 }
